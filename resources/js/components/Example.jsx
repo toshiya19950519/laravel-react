c284596b76c76626bd345df77fc58ceea9710bc6
Toshiya Takahashi <toshiya2113@gmail.com>
2023-09-18 09:36:30 +0900

    Material-UIの導入

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -1,15 +1,17 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Example() {
+function App() {
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header">導入できたお</div>
 
                         <div className="card-body">I'm an example component!</div>
+                        <Button color="primary" variant="contained">Hello World</Button>
                     </div>
                 </div>
             </div>
@@ -17,14 +19,14 @@ function Example() {
     );
 }
 
-export default app;
+export default App;
 
 if (document.getElementById('app')) {
     const Index = ReactDOM.createRoot(document.getElementById("app"));
 
     Index.render(
         <React.StrictMode>
-            <Example/>
+            <App/>
         </React.StrictMode>
     )
 }
